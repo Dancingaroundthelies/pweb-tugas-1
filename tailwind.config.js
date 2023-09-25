4ced201f51268c6254e03e3083f0df70318337e4
Ra <91133817+Dancingaroundthelies@users.noreply.github.com>
2023-09-25 20:28:23 +0700

    up final

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'cerulean blue': '#284B63',
+        'dark sky': '#353535',
+      },
+    },
+  },
+  plugins: [],
+}
